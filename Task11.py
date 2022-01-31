77039b73350b720c9f0681062645e87cc2465ede
AntonUmarov <arumarov@yandex.ru>
2022-02-01 00:23:13 +0600

    Task11

---
+++ Task11.py
@@ -0,0 +1,14 @@
+# Задача 11. Сформировать список из  N членов последовательности.
+# Для N = 5: 1, -3, 9, -27, 81 и т.д.
+
+def create_list(n):
+    a = []
+    a.append(1)
+    res = 1
+    for i in range(1, n):
+        res = res*(-3) 
+        a.append(res) 
+    print(a)
+print(create_list(6))
+
+
