e017189d4dfc55fd049ec48d01cb6b979065e68b
AntonUmarov <arumarov@yandex.ru>
2022-02-07 17:18:28 +0600

    Task14.Webinar

--- Task14.py
+++ Task14.py
@@ -1,4 +1,5 @@
 # Задача 14. Подсчитать сумму цифр в вещественном числе
+# Мое решение
 def sum_digits(a):
     b = str(a)
     c = b.replace('.', '')
@@ -7,3 +8,11 @@ def sum_digits(a):
         sum = int(c[i]) + sum
     return sum
 print(sum_digits(697.18755))
+
+# Решение с семинара
+def sum_of_number(a):
+    res = 0
+    for i in str(a):
+        if i != '.': res += int(i)
+    return res
+print(sum_of_number(697.18755))
